b8ddedaf940ea606d59b17c5e1d94ac71faeda70
onlineAV8or <linkedin.ak.100@gmail.com>
2025-04-14 03:57:02 +0530

    3d text sample edited

--- text3D/src/script.js
+++ text3D/src/script.js
@@ -52,7 +52,6 @@ fontLoader.load(
 
         const donutGeometry =  new THREE.TorusGeometry(0.3,0.2,20,45)
         const donutMaterial = new THREE.MeshMatcapMaterial({matcap: matcapTextureMetallic})
-        //const donutMaterial = new THREE.MeshNormalMaterial()
         for(let i=0;i<100;i++)
         {
             const donut = new THREE.Mesh(donutGeometry, donutMaterial)
@@ -73,86 +72,7 @@ fontLoader.load(
 )
 
 
-// const doorAlphaTexture = textureLoader.load('/textures/door/alpha.jpg')
-// const doorColorTexture = textureLoader.load('/textures/door/color.jpg')
-// const doorAmbientTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg')
-// const doorHeightTexture = textureLoader.load('/textures/door/height.jpg')
-// const doorMetalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
-// const doorNormalTexture = textureLoader.load('/textures/door/normal.jpg')
-// const doorRoughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
-// //const matCapTexture = textureLoader.load('/textures/matcaps/3.png')
-// const gradientTexture = textureLoader.load('/textures/gradients/3.jpg')
 
-
-// const environmentMapTexture = cubeTextureLoader.load(
-//     [
-//         '/textures/environmentMaps/0/px.jpg',
-//         '/textures/environmentMaps/0/nx.jpg',
-//         '/textures/environmentMaps/0/py.jpg',
-//         '/textures/environmentMaps/0/ny.jpg',
-//         '/textures/environmentMaps/0/pz.jpg',
-//         '/textures/environmentMaps/0/nz.jpg',
-//     ]
-// )
-
-
-/**
- * Object
- */
-// const material = new THREE.MeshBasicMaterial(); 
-// material.map = doorColorTexture
-// material.alphaMap = doorAlphaTexture
-// material.transparent = true
-// material.side = THREE.DoubleSide
-
-// const material= new THREE.MeshNormalMaterial()
-// material.flatShading=true
-
-// const material = new THREE.MeshMatcapMaterial()
-// material.matcap = matCapTexture
-//const material = new THREE.MeshDepthMaterial()
-// const material = new THREE.MeshStandardMaterial()
-// material.metalness = 0.7
-// material.roughness = 0.2
-// material.envMap = environmentMapTexture
-// material.side = THREE.DoubleSide
-
-
-// material.side = THREE.DoubleSide
-// material.map = doorColorTexture
-// material.aoMap = doorAmbientTexture
-// material.displacementMap = doorHeightTexture
-// material.displacementScale = 0.1
-// material.metalnessMap = doorMetalnessTexture
-// material.normalMap = doorNormalTexture
-// material.alphaMap = doorAlphaTexture
-// material.transparent = true
-//mate
-// gui.add(material, 'metalness').min(0).max(1).step(0.0001)
-// gui.add(material,'roughness').min(0).max(1).step(0.0001)
-// gui.add(material,'aoMapIntensity').min(0).max(10).step(0.0001)
-
-
-// const sphere = new THREE.Mesh( 
-//     new THREE.SphereGeometry( 0.5, 16, 16 ), 
-//     material
-// );
-// sphere.position.x = -1.5
-
-// const plane = new THREE.Mesh( 
-//     new THREE.PlaneGeometry(1,1), 
-//     material
-// ); 
-// plane.position.x = 1.5
-// plane.geometry.setAttribute('uv2',new THREE.BufferAttribute(plane.geometry.attributes.uv.array,2))
-
-
-// const torus = new THREE.Mesh(
-//     new THREE.TorusGeometry(0.3,0.2,16.32),
-//     material
-// )
-
-//scene.add( sphere,plane,torus )
 
 /**
  * Light
@@ -215,10 +135,6 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
-
-    // sphere.rotation.y = 0.3*elapsedTime
-    // torus.rotation.y = 0.3*elapsedTime
-    // plane.rotation.y = 0.3*elapsedTime
 
     // Update controls
     controls.update()
