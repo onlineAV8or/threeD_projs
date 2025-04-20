040e8065ee0997931be1dd21ba5f2b542d7cd585
onlineAV8or <linkedin.ak.100@gmail.com>
2025-04-20 23:06:41 +0530

    galaxy gen

---
+++ galaxy/vite.config.js
@@ -0,0 +1,18 @@
+const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env
+
+export default {
+    root: 'src/',
+    publicDir: '../static/',
+    base: '/threeD_projs/galaxy/',
+    server:
+    {
+        host: true,
+        open: !isCodeSandbox // Open if it's not a CodeSandbox
+    },
+    build:
+    {
+        outDir: '../../dist/galaxy/',
+        emptyOutDir: true,
+        sourcemap: true
+    }
+}
